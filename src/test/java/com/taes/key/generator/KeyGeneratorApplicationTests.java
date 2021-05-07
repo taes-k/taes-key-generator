ae7416185aa9cd33c7d053cedd4938d9b30d748f
taes <cobura12@gmail.com>
2021-05-07 21:47:02 +0900

    project init

---
+++ src/test/java/com/taes/key/generator/KeyGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taes.key.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
